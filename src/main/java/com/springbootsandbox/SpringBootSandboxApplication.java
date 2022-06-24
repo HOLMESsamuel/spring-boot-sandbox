4b16df8e1d394563de4ffac7d64a44a7646621eb
Samuel Holmes <sam.holmes@live.fr>
2022-06-24 15:10:02 +0200

    add command line runner

--- src/main/java/com/springbootsandbox/SpringBootSandboxApplication.java
+++ src/main/java/com/springbootsandbox/SpringBootSandboxApplication.java
@@ -1,13 +1,18 @@
 package com.springbootsandbox;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootSandboxApplication {
+public class SpringBootSandboxApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootSandboxApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Command line runner executed");
+    }
 }
