06df3ee98fa8c1ccc4e435f401dbbfb4699547dc
Samuel Holmes <47476255+HOLMESsamuel@users.noreply.github.com>
2022-07-20 14:19:37 +0200

    add code tangling logging for demo

--- src/main/java/com/springbootsandbox/service/CatService.java
+++ src/main/java/com/springbootsandbox/service/CatService.java
@@ -3,17 +3,20 @@ package com.springbootsandbox.service;
 import com.springbootsandbox.entity.Cat;
 import com.springbootsandbox.repository.CatRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class CatService {
 
     private CatRepository catRepository;
 
     public List<Cat> getAllCats() {
+        log.info("getting cats");
         return catRepository.findAll();
     }
 
